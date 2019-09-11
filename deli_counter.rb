db2ad547fd3338ca1a79f496da30db14a9d4845c
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-11 09:20:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,22 +5,32 @@
     else
       str = ""
       deli.each_with_index do |ele,idx|
-        if idx == deli.length - 1
-          str += "#{idx+1}. #{ele}"
-        else
-          str += "#{idx+1}. #{ele} "
-        end
+        # if idx == deli.length - 1
+        #   str += "#{idx+1}. #{ele}"
+        # else
+          str += " #{idx+1}. #{ele}"
+        #end
       end
-      puts "The line is currently: #{str}"
+     # puts "The line is currently: #{str}"
+     puts "The line is currently:#{str}"
     end
   end
   
-  def take_a_number(deli, pname)
-    deli << pname
-    puts "Welcome, #{pname}. You are number #{deli.length} in line."
-  end
+  # def take_a_number(deli, pname) #[1,2,3,4]
+  #   deli << pname
+  #   puts "Welcome, #{pname}. You are number #{deli.length} in line."
+  # end
   
-  def now_serving(deli)
+   ticket_num = 0 
+  def take_a_number(deli) #[1,
+    
+    ticket_num += 1
+    deli << ticket_num 
+    puts "Welcome! You have ticket number #{ticket_num}."
+    # deli << [deli.length + 1] # [2,3,4,5]
+  end 
+  
+  def now_serving(deli) #[2,3,4,5]
     if deli.empty?
       puts "There is nobody waiting to be served!"
     else
